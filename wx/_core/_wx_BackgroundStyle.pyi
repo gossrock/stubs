8531fcb668847cc89dec7af973d6afbd77387193
gossrock <gossrock@gmail.com>
2018-10-18 11:01:31 +0000

    create BackgroundStyle Constant class

---
+++ wx/_core/_wx_BackgroundStyle.pyi
@@ -0,0 +1,7 @@
+class BackgroundStyle(int): ...
+
+BG_STYLE_ERASE: BackgroundStyle
+BG_STYLE_SYSTEM: BackgroundStyle
+BG_STYLE_PAINT: BackgroundStyle
+BG_STYLE_COLOUR: BackgroundStyle
+BG_STYLE_TRANSPARENT: BackgroundStyle
