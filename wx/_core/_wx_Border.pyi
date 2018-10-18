4121896cc35fb9526597b44f5612a6b87ef4c376
gossrock <gossrock@gmail.com>
2018-10-18 11:01:54 +0000

    created Border Constant class

---
+++ wx/_core/_wx_Border.pyi
@@ -0,0 +1,11 @@
+class Border(int): ...
+
+BORDER_DEFAULT: Border
+BORDER_NONE: Border
+BORDER_STATIC: Border
+BORDER_SIMPLE: Border
+BORDER_RAISED: Border
+BORDER_SUNKEN: Border
+BORDER_DOUBLE: Border
+BORDER_THEME: Border
+BORDER_MASK: Border
